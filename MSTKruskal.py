cf4313e84e510f7b79e1fc60dcc134a1df5cc291
ch <chlgur13@kakao.com>
2023-12-08 00:39:42 +0900

    add Prim algorithm

--- MSTKruskal.py
+++ MSTKruskal.py
@@ -20,4 +20,29 @@ def union(s1, s2):
     global set_size
     parent[s1] = s2
     set_size = set_size -1
-    
+
+
+def MSTKruskal(vertex, adj): #매개변수 : 정점리스트, 인접행렬
+    vsize = len(vertex)      #정점의 개수
+    init_set(vsize)          #정점 집합 초기화
+    eList = []               #간선 리스트
+
+    for i in ragne(vsize-1) :         
+        for j in range(i+1, vsize) :
+            if adj[i][j] != None :
+                eList.append((i,j,adj[i][j]))
+
+    #간선 리스트를 가중치의 내림차순으로 정렬:람다 함수 사용
+    eList.sort(key = lambda e : e[2], reverse=True)
+
+    edgeAccepted = 0
+    while(edgeAccepted < vsize - 1):
+        e = eList.pop()
+        uset = find(e[0])
+        vset = find(e[1])
+
+        if uset != vset :
+            print("간선 추가 : (%s,%s,%d)" %
+                  (vertex[e[0]],vertex[e[1]],e[2]))
+            union(uset,vset)
+            edgeAccepted += 1
